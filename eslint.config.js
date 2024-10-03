c0ba8d6db5da8c2ae3d273af96a7c3cd34dda075
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-03 17:18:45 +0200

    1

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,13 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
+	{
+		compilerOptions: {
+			target: "es6",
+			jsx: "react",
+		},
+		include: ["src/**/*"],
+	},
 	{ ignores: ["dist"] },
 	{
 		files: ["**/*.{js,jsx}"],
